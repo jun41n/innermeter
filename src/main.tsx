e77d833301b5cfd0ab18212b30df7112203635c1
jun41n <jun41n@gmail.com>
2026-03-19 22:00:38 +0900

    처음부터 다시

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { Routes } from './routes';
-import './index.css';
-import './lib/i18n'; // Import the i18n configuration
-
-createRoot(document.getElementById('root')!).render(<Routes />);
